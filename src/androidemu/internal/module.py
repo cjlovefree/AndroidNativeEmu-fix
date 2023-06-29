66d5a370af75182738c4635a8a8d3929fb7422c9
cjlovefree <121709972+cjlovefree@users.noreply.github.com>
2023-06-29 15:27:52 +0800

    Update module.py

--- src/androidemu/internal/module.py
+++ src/androidemu/internal/module.py
@@ -28,4 +28,13 @@ class Module:
             return self.symbol_lookup[addr](0)
         else:
             return None
+            
+    def call_init(self, emu):
+        for fun_ptr in self.init_array:
+            fun_addr = fun_ptr
+            print("Calling Init_array %s function: 0x%08X " %(self.filename, fun_addr))
+            emu.call_native(fun_addr)
+
+
+
 
